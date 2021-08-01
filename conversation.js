0dfff94c9dfda44992ccbd931c30acc97beb3e39
labibkhanofficialbd <labibkhan66410@gmail.com>
2021-08-01 18:01:14 +0600

    intro to js

---
+++ conversation.js
@@ -0,0 +1,28 @@
+
+
+var firstName = "Ariana";
+var lastName = "Grande"
+
+var fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
+
+
+// string covert to int and float 
+
+var eggPrice = '42';
+var oinionPrice ='43'
+
+var oinionPriceNumber = parseInt(oinionPrice);
+var eggPriceNumber = parseInt(eggPrice);
+
+console.log(oinionPriceNumber + eggPriceNumber);
+
+
+var eggPrice = '42.33';
+var oinionPrice ='43.13'
+
+var oinionPriceNumber = parseFloat(oinionPrice);
+var eggPriceNumber = parseFloat(eggPrice);
+
+console.log(oinionPriceNumber + eggPriceNumber);
